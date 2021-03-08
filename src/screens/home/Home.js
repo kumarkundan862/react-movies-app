c467a18c50b7d294fc4f84b1c445feac25627ac6
kumarkundan862 <kumarkundan862@gmail.com>
2021-03-08 08:04:49 +0530

    Added grid list for 'Released Movies' on home page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -22,6 +22,11 @@ const styles = theme => ({
         flexWrap: 'nowrap',
         transform: 'translateZ(0)',
         width: '100%'
+    },
+
+    gridListMain: {
+        transform: 'translateZ(0)',
+        cursor: 'pointer'
     }
 });
 
@@ -37,14 +42,32 @@ class Home extends Component {
                 <GridList cols={5} className={classes.gridListUpcomingMovies}>
                     {moviesData.map(movie => (
                         <GridListTile key={movie.id}>
-                            <img src={movie.poster_url} alt={movie.title} />
+                            <img src={movie.poster_url} className="movie-poster" alt={movie.title} />
                             <GridListTileBar title={movie.title} />
-                        </GridListTile>    
+                        </GridListTile>
                     ))}
-                </GridList>    
+                </GridList>
+
+                <div className="flex-container">
+                    <div className="left">
+                        <GridList cellHeight={350} cols={4} className={classes.gridListMain}>
+                            {moviesData.map(movie => (
+                                <GridListTile className="released-movie-grid-item" key={"grid" + movie.id}>
+                                    <img src={movie.poster_url} className="movie-poster" alt={movie.title} />
+                                    <GridListTileBar
+                                        title={movie.title}
+                                        subtitle={<span>Release Date: {new Date(movie.release_date).toDateString()}</span>}
+                                    />
+                                </GridListTile>
+                            ))}
+                        </GridList>
+                    </div>
+                    <div className="right">
+                    </div>
+                </div>
             </div>
         )
     }
 }
 
-export default withStyles(styles)(Home);
+export default withStyles(styles)(Home);    
